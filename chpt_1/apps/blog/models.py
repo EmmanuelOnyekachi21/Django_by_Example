300fdd0a4f71a59887a6b56fc34ebe7c6cfa9c41
Nnabugwu Emmanuel <emmanuelonyekachi04122000@gmail.com>
2026-02-01 04:06:53 +0100

    feat(blog): Add commenting system and post sharing functionality
    - Create Comment model with ForeignKey relationship to Post
    - Add CommentAdmin to Django admin with list display and filtering
    - Create EmailForm for sharing posts and CommentForm for adding comments
    - Add get_absolute_url() method to Post model for canonical URLs
    - Create comment detail template and comment form include template
    - Add share post template for email sharing functionality
    - Create pagination template for post list navigation
    - Update post detail template to display comments and comment form
    - Update post list template to use get_absolute_url() for post links
    - Register django_extensions in settings for enhanced functionality
    - Generate migration for Comment model and Post slug field updates
    - Enable users to comment on blog posts and share posts via email

--- chpt_1/apps/blog/models.py
+++ chpt_1/apps/blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.conf import settings
 from django.utils.text import slugify
 import uuid
+from django.urls import reverse
 
 # Create your models here.
 class PublishedManager(models.Manager):
@@ -65,3 +66,37 @@ class Post(models.Model):
             self.slug = slug
 
         super().save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse(
+            "post_detail",
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created'])
+        ]
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
+
--- chpt_1/apps/blog/urls.py
+++ chpt_1/apps/blog/urls.py
@@ -4,5 +4,15 @@ from . import views
 urlpatterns = [
     # Post views
     path('', views.post_list, name='post_list'),
-    path('<uuid:id>/', views.post_detail, name='post_detail'),
+    path(
+        '<int:year>/<int:month>/<int:day>/<slug:post>/',
+        views.post_detail,
+        name='post_detail'
+    ),
+    path('<uuid:post_id>/share/', views.post_share, name="post_share"),
+    path(
+        '<uuid:post_id>/comment/',
+        views.post_comment,
+        name="post_comment"
+    ),
 ]
